a020bec423bb3bc96c4371de773735c129c7a728
su57ks <samuray.ks@yandex.ru>
2026-05-13 16:33:13 +0300

    Add files via upload

---
+++ desktop_client.py
@@ -0,0 +1,115 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 1000))
+font = pygame.font.SysFont(None, 36)
+
+class Button():
+    def __init__(self, pText, pStandart_color, pClick_color, pPosition, pFont):
+        self.font = pFont
+        self.text = self.font.render(pText, True, (255, 255, 255))
+        self.standart_color = pStandart_color
+        self.click_color = pClick_color
+        self.position = pygame.Rect(pPosition)
+        self.current_color = self.standart_color
+
+        self.clicked = False
+
+    def update(self, events):
+        self.clicked = False
+        mouse_pos = pygame.mouse.get_pos()
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.position.collidepoint(mouse_pos):
+                self.clicked = True
+            
+        if self.position.collidepoint(mouse_pos):
+            self.current_color = self.click_color
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.current_color, self.position, border_radius=8)
+        text_rect = self.text.get_rect(center=self.position.center)
+        screen.blit(self.text, text_rect)
+        self.current_color = self.standart_color
+
+class TextField():
+    def __init__(self, pColor, pPosition, pFont):
+        self.font = pFont
+        self.color = pColor
+        self.position = pygame.Rect(pPosition)
+        self.text = ""
+
+    def update(self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+
+    def draw(self, screen):
+        text = self.font.render(self.text, True, (255, 255, 255))
+        pygame.draw.rect(screen, self.color, self.position, border_radius=8)
+        text_rect = text.get_rect(center=self.position.center)
+        screen.blit(text, text_rect)
+          
+class Modal():
+    def __init__(self, pColor, pPosition, pFont, pText):
+        self.font = pFont
+        self.color = pColor
+        self.position = pygame.Rect(pPosition)
+        self.text = self.font.render(pText, True, (255, 255, 255))
+        self.showed = True
+
+        self.close = Button("х", (70, 130, 180), (255, 0, 0), (pPosition[1] + pPosition[2] - 30, pPosition[0], 30, 30), self.font)
+
+    def update(self, events):
+        self.close.update(events)
+        if self.close.clicked:
+            self.showed = False
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.position, border_radius=8)
+        self.close.draw(screen)
+        text_rect = self.text.get_rect(center=self.position.center)
+        screen.blit(self.text, text_rect)
+
+settings = Button("Настройки", (70, 130, 180), (0, 160, 00), (0, 0, 200, 100), font)
+
+input = TextField((0, 160, 00), (0, 200, 200, 100), font)
+
+running = True
+modal_showing = False
+modal = None
+while running:
+    mouse_pos = pygame.mouse.get_pos()
+
+    events = pygame.event.get()
+
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((30, 30, 30))
+        
+    settings.update(events)
+    if settings.clicked:
+        print("Кнопка настроек нажата!")
+        modal_showing = True
+        modal = Modal((100, 100, 100), (300, 300, 200, 100), font, "нету")
+    settings.draw(screen)
+
+    if modal_showing == True:
+        modal.update(events)
+        modal.draw(screen)
+
+        if modal.showed == False:
+            modal_showing = False
+            modal = None
+
+    input.update(events)
+    input.draw(screen)
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
