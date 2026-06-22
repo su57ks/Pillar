ec9a62eafccd74ad286aef7acf5ebda43a28bc1f
su57ks <samuray.ks@yandex.ru>
2026-06-22 16:03:47 +0300

    create TextField and Button

--- android/app/src/main/java/com/example/pillar/ui/screen/ChatScreen.kt
+++ android/app/src/main/java/com/example/pillar/ui/screen/ChatScreen.kt
@@ -5,20 +5,35 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,6 +43,9 @@ import com.example.pillar.utils.messageTime
 
 @Composable
 fun ChatScreen(viewModel: MainViewModel = viewModel()) {
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val maxHeight = screenHeight * 0.05f
+    var input by remember { mutableStateOf("") }
     val id by viewModel.selectedId.collectAsState()
     Log.d("UPDATE ID", id.toString())
     val chatDict by viewModel.chatDict.collectAsState()
@@ -43,8 +61,40 @@ fun ChatScreen(viewModel: MainViewModel = viewModel()) {
             for (message in chat.messages) {
                 Message(message = message, viewModel = viewModel)
             }
+            Spacer(modifier = Modifier.height(maxHeight + 2.dp))
         }
         Text(text = chat.user.name)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .background(Color.Green)
+                    .clip(RoundedCornerShape(20.dp)),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                TextField(
+                    value = input,
+                    onValueChange = { input = it },
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(maxHeight),
+                    shape = RoundedCornerShape(20.dp)
+                )
+                Button(
+                    onClick = {}
+                ) {
+                    Icon(
+                        Icons.Default.PlayArrow,
+                        contentDescription = ""
+                    )
+                }
+            }
+        }
     }
 }
 
