26581609cdec1fb306b2e8aaa2f896d6fccf0c91
su57ks <samuray.ks@yandex.ru>
2026-06-22 13:02:54 +0300

    fix VM implementation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -56,5 +56,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.9.8")
-    implementation("androidx.lifecycle:lifecycle-viewmodelcompose:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
 }
